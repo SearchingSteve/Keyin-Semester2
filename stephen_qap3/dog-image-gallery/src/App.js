4b06a8349c2d898f1b5c60cbb150dd5db637b047
SearchingSteve <155838264+SearchingSteve@users.noreply.github.com>
2024-08-11 15:20:55 -0230

    Fix stephen_qap3 - dog-image-gallery
    Update App.js to have correct code

--- stephen_qap3/dog-image-gallery/src/App.js
+++ stephen_qap3/dog-image-gallery/src/App.js
@@ -1,22 +1,56 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Navigation from './Navigation';
-import Home from './Home';
-import RandomAdvice from './RandomAdvice';
-import AdviceList from './AdviceList';
+import React, { useState, useEffect } from 'react';
+import BreedSelector from './components/BreedSelector';
+import ImageGallery from './components/ImageGallery';
+import './App.css';
 
 function App() {
+  const [breeds, setBreeds] = useState([]);
+  const [selectedBreed, setSelectedBreed] = useState('');
+  const [numImages, setNumImages] = useState(1);
+  const [images, setImages] = useState([]);
+
+  useEffect(() => {
+    fetch('https://dog.ceo/api/breeds/list/all')
+      .then(response => response.json())
+      .then(data => {
+        setBreeds(Object.keys(data.message));
+      })
+      .catch(error => console.error('Error fetching breeds:', error));
+  }, []);
+
+  const fetchImages = () => {
+    if (selectedBreed && numImages > 0 && numImages <= 100) {
+      fetch(`https://dog.ceo/api/breed/${selectedBreed}/images/random/${numImages}`)
+        .then(response => response.json())
+        .then(data => {
+          setImages(data.message);
+        })
+        .catch(error => console.error('Error fetching images:', error));
+    }
+  };
+
   return (
-    <Router>
-      <div>
-        <Navigation />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/random-advice" element={<RandomAdvice />} />
-          <Route path="/advice-list" element={<AdviceList />} />
-        </Routes>
-      </div>
-    </Router>
+    <div className="App">
+      <h1>Dog Image Gallery</h1>
+      <BreedSelector
+        breeds={breeds}
+        selectedBreed={selectedBreed}
+        setSelectedBreed={setSelectedBreed}
+        numImages={numImages}
+        setNumImages={setNumImages}
+        fetchImages={fetchImages}
+      />
+      <br></br>
+      {images.length === 0 ? (
+        <p>Try Me Now!
+          <br></br>
+          <br></br>
+          Select your favorite breed and number of images to see a gallery of dog pictures.
+        </p>
+      ) : (
+        <ImageGallery images={images} />
+      )}
+    </div>
   );
 }
 
